5e185e1dc95e692ea78e917d2a3f645f111fdbea
Rafael Milewski <rafael.milewski@gmail.com>
2023-06-01 11:25:57 +0800

    add class opening fixer

--- src/Custom.php
+++ src/Custom.php
@@ -4,6 +4,7 @@ declare (strict_types = 1);
 
 namespace DigitalCreative\ECS;
 
+use DigitalCreative\ECS\Fixers\ClassOpeningBracketFixer;
 use DigitalCreative\ECS\Fixers\PaddedArrayFixer;
 use DigitalCreative\ECS\Fixers\PaddedBlockFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
@@ -12,7 +13,8 @@ return static function (ECSConfig $config): void {
 
     $options = [
         PaddedArrayFixer::class => true,
-        PaddedBlockFixer::class => true
+        PaddedBlockFixer::class => true,
+        ClassOpeningBracketFixer::class => true,
     ];
 
     register_fixers($config, $options);
